8ba37176b604bfe4198c10d8920646e93321011c
Akmal Ahmed <akmal.ahmed@itelasoft.com>
2024-10-26 19:40:57 +0530

    Initial Commit :tada:
    Created the Flutter project (v3.24.4)

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_advanced
+
+A POC application to learn Animations & Advanced Concepts in Flutter
---
+++ android/app/src/main/kotlin/au/com/itelasoft/flutter_advanced/MainActivity.kt
@@ -0,0 +1,5 @@
+package au.com.itelasoft.flutter_advanced
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
